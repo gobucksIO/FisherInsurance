c9d80f2fde8e01255d17cf4e4678b87ede5b7ed6
Trevor Kononenko <Trevor Kononenko>
2017-02-08 18:00:22 -0500

    Claims and Quote Model Push

---
+++ Models/Claims.cs
@@ -0,0 +1,17 @@
+Using System;
+
+namespace FisherInsurance.Models;
+{
+
+    public class Claims{
+        public int Id{get; set;}
+
+        public string PolicyType{get; set;}
+
+        public DateTime LossDate{get; set;}
+
+        public decimal LossAmount{get; set;}
+
+    }
+    
+}
